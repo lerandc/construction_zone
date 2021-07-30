158608a8977ac6664c81aab75739f7007359fd43
Luis Rangel DaCosta <luisrd@berkeley.edu>
2021-07-30 15:13:09 -0700

    document Scene class

--- czone/scene/scene.py
+++ czone/scene/scene.py
@@ -1,15 +1,20 @@
-"""
-Scene Class
-Luis Rangel DaCosta
-"""
-
 from ..volume import BaseVolume, makeRectPrism
 import numpy as np
 from ase import Atoms
-from ase.symbols import Symbols, string2symbols
+from ase.symbols import Symbols
 from ase.io import write as ase_write
+from typing import List
 
 class Scene():
+    """Scene classes manage multiple objects interacting in space with cell boundaries.
+
+    Attributes:
+        bounds (np.ndarray): 2x3 array defining rectangular bounds of scene.
+        objects (List[BaseVolume]): list of all objects currently in scene.
+        all_atoms (np.ndarray): coordinates of all atoms in scene after precedence checks.
+        all_species (np.ndarray): atomic numbers of all atoms in scene after precedence checks.
+        ase_atoms (Atoms): collection of atoms in scene as ASE Atoms object
+    """
 
     def __init__(self, bounds=None, objects=None):
         self._bounds = None
@@ -32,19 +37,26 @@ class Scene():
 
     @property
     def bounds(self):
+        """Current boundaries of nanoscale scene."""
         return self._bounds
 
     @bounds.setter
-    def bounds(self, bounds):
+    def bounds(self, bounds: np.ndarray):
         bounds = np.array(bounds)
         assert(bounds.shape == (2,3))
         self._bounds = bounds
 
     @property
     def objects(self):
+        """List of objects in current scene."""
         return self._objects
 
-    def add_object(self, ob):
+    def add_object(self, ob: BaseVolume):
+        """Add an object to the scene.
+        
+        Args:
+            ob (BaseVolume): object to add to scene.
+        """
         #for now, only volumes are objects
         if isinstance(ob, BaseVolume):
             if self._objects is None:
@@ -53,26 +65,44 @@ class Scene():
                 self._objects.append(ob)
 
     @property
-    def checks(self):
+    def _checks(self):
+        """List of logical arrays indicating inclusion of atoms in scene from each object."""
         return self._checks
 
     @property
     def all_atoms(self):
-        return np.vstack([ob.atoms[self.checks[i],:] for i, ob in enumerate(self.objects)])
+        """Positions of all atoms currently in the scene after evaluating conflict resolution."""
+        return np.vstack([ob.atoms[self._checks[i],:] for i, ob in enumerate(self.objects)])
 
     @property
     def all_species(self):
-        return np.hstack([ob.species[self.checks[i]] for i, ob in enumerate(self.objects)])
+        """Atomic numbers of all atoms currently in the scene after evaluating conflict resolution."""
+        return np.hstack([ob.species[self._checks[i]] for i, ob in enumerate(self.objects)])
 
     @property
     def ase_atoms(self):
+        """Collection of atoms in scene as ASE Atoms object."""
         cell_dims = self.bounds[1,:]-self.bounds[0,:]
         return Atoms(symbols=self.all_species, positions=self.all_atoms, cell=cell_dims)
 
-    def species_from_object(self, idx):
-        return self.objects[idx].atoms[self.checks[idx],:]
+    def species_from_object(self, idx: int):
+        """Grab all the atoms from contributing object at idx.
+        
+        Returns:
+            Numpy array of all positions of atoms contributed by object at idx.
+        """
+        return self.objects[idx].atoms[self._checks[idx],:]
 
-    def get_priorities(self):
+    def _get_priorities(self):
+        """Grab priority levels of all objects in Scene to determine precedence relationship.
+
+        Returns:
+            List of relative priority levels and offsets. Relative priority levels
+            and offsets are used to determine which objects whill be checked
+            for the inclusion of atoms in the scene of the atoms contributed by
+            another object.
+        
+        """
         # get all priority levels active first
         self.objects.sort(key=lambda ob: ob.priority)
         plevels = np.array([x.priority for x in self.objects]) 
@@ -88,25 +118,21 @@ class Scene():
         return rel_plevels, offsets
 
     def populate(self):
-        """
-        First, every object populates atoms against its own boundaries
+        """Populate the scene with atoms according to Volumes and priority levels.
 
-        Then, gather the list of priorities from all the objects
-        For each object:
-            1) Generate a True array of length ob.atoms
-            2) For each object in the same priority level or lower, perform
-                interiority check and repeatedly perform logical_and to see if
-                atoms belong in scene
+        First, every object populates atoms against its own boundaries.
+        Then, gather the list of priorities from all the objects.
+        For each object, generate a True array of length ob.atoms. 
+        For each object in the same priority level or lower, perform interiority 
+        check and repeatedly perform logical_and to see if atoms belong in scene.
 
-        
-        -> Lower priority numbers supercede objects with high priority numbers
-        -> Objects on the same priority level will not supply atoms to the scene 
-            in their volume intersections
+        - Lower priority numbers supercede objects with high priority numbers.
+        - Objects on the same priority level will not supply atoms to the scene in their volume intersections.
         """
         for ob in self.objects:
             ob.populate_atoms()
 
-        rel_plevels, offsets = self.get_priorities()
+        rel_plevels, offsets = self._get_priorities()
 
         self._checks = []
 
@@ -122,6 +148,16 @@ class Scene():
             self._checks.append(check)
 
     def to_file(self, fname, **kwargs):
+        """Write atomic scene to an output file, using ASE write utilities.
+
+        If format="prismatic", will default to Debye-Waller factors of 0.1 RMS 
+        displacement in squared angstroms, unless dictionary of debye-waller factors
+        is otherwise supplied.
+        
+        Args:
+            fname (str): output file name.
+            **kwargs: any key word arguments otherwise accepted by ASE write.
+        """
         if "format" in kwargs.keys():
             if kwargs["format"] == "prismatic":
                 dwf = set(self.all_species)
