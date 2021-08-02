c6f03870cb131f730409d971997c404c70bdf795
Luis Rangel DaCosta <luisrd@berkeley.edu>
2021-08-02 14:51:48 -0700

    path test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0,os.path.dirname(os.getcwd()))
+sys.path.insert(0,"czone/")
 
 # -- Project information -----------------------------------------------------
 
