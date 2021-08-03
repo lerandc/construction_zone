6849e4bdf0587a4e2b93b8981a446d9e9a618422
Luis Rangel DaCosta <luisrd@berkeley.edu>
2021-08-02 18:49:09 -0700

    try color change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+def setup(app):
+    app.add_css_file('custom.css')
+    
 import os
 import sys
 sys.path.insert(0,"../../")
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,6 +1,8 @@
 Construction Zone
 =============================================
 
+Hello!
+
 .. toctree::
    :maxdepth: 4
    :caption: Contents
