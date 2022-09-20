ba824215ef449c66833a127707228dab364b9092
Luis Rangel DaCosta <luisrd@berkeley.edu>
2022-09-20 10:38:56 -0700

    versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='czone',
-    version='2022.09.19',
+    version='2022.09.20',
     description='An open source python package for generating nanoscale+ atomic scenes',
     url='https://github.com/lerandc/construction_zone',
     author='Luis Rangel DaCosta',
